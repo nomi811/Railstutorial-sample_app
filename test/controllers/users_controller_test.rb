d7b1a59ccffb0c1903739a169431f24c55c1b00d
Nomi Vos <nvoswork@gmail.com>
2017-04-19 05:33:21 -0500

    changes to make header run properly and correct test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,11 +5,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get new" do
     get signup_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "Sign up | #{@base_title}"
   end
 
 end
