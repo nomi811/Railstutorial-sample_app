499cc93ad4ec78cb9d5968791fbd8a736c3c4899
Nomi Vos <nvoswork@gmail.com>
2017-05-04 05:07:59 -0500

    10.4.3 User destroy tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,15 +3,15 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
     @user = users(:michael)
     @other_user = users(:archer)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get signup_path
     assert_response :success
-    assert_select "title", "Sign up | #{@base_title}"
+    assert_select 'title', "Sign up | #{@base_title}"
   end
 
   test 'should redirect edit when logged in as wrong user' do
@@ -46,8 +46,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@other_user), params: {
                                     user: { password:               'password',
                                             password_confirmation:  'password',
-                                            admin: true } }
+                                            admin: true }
+    }
     assert_not @other_user.reload.admin?
   end
 
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
